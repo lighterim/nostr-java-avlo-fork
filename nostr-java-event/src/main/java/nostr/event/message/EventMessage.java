e4fa38f5754a6eba5535476412d6850207e378f3
yuepeng <yuepeng@yuepengs-MacBook-Pro.local>
2024-08-12 00:12:14 +0800

    nostr/event/message/EventMessage.java add subscirptionId

--- nostr-java-event/src/main/java/nostr/event/message/EventMessage.java
+++ nostr-java-event/src/main/java/nostr/event/message/EventMessage.java
@@ -3,7 +3,9 @@ package nostr.event.message;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -39,11 +41,32 @@ public class EventMessage extends BaseMessage {
 
     @Override
     public String encode() throws JsonProcessingException {
-        return IEncoder.MAPPER.writeValueAsString(
-            getArrayNode()
-                .add(getCommand())
-                .add(IEncoder.MAPPER.readTree(
-                    new BaseEventEncoder<>((BaseEvent)getEvent()).encode())));
+//        return IEncoder.MAPPER.writeValueAsString(
+//            getArrayNode()
+//                .add(getCommand())
+//                .add(IEncoder.MAPPER.readTree(
+//                    new BaseEventEncoder<>((BaseEvent)getEvent()).encode())));
+//
+        ArrayNode arrayNode = IEncoder.MAPPER.createArrayNode();
+
+        // 将命令添加到 JSON 数组中
+        arrayNode.add(this.getCommand());
+
+        // 将 subscriptionId 添加到 JSON 数组中
+        if (this.subscriptionId != null) {
+            arrayNode.add(this.subscriptionId);
+        } else {
+            // 如果 subscriptionId 为空，添加一个空字符串
+            arrayNode.add("");
+        }
+
+        // 将事件编码为 JSON 并添加到数组中
+        String eventJson = new BaseEventEncoder((BaseEvent)this.getEvent()).encode();
+        JsonNode eventNode = IEncoder.MAPPER.readTree(eventJson);
+        arrayNode.add(eventNode);
+
+        // 将数组编码为字符串并返回
+        return IEncoder.MAPPER.writeValueAsString(arrayNode);
     }
 
     public static <T extends BaseMessage> T decode(@NonNull Object[] msgArr, ObjectMapper mapper) {

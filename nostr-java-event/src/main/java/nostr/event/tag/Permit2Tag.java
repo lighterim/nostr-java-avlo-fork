43930d9b730457cdba95e8e1de9ba534dd5afe34
Dust <dust@live.com>
2025-10-23 22:26:35 +0800

    permit2.spender

--- nostr-java-event/src/main/java/nostr/event/tag/Permit2Tag.java
+++ nostr-java-event/src/main/java/nostr/event/tag/Permit2Tag.java
@@ -22,15 +22,21 @@ public class Permit2Tag extends BaseTag {
     private final String nonce;
     private final String signature;
     private final String payer;
+    private final String spender;
 
     public static <T extends BaseTag> T deserialize(@NonNull JsonNode node) {
-        final String nonce = Optional.ofNullable(node.get(1)).orElseThrow().asText();
-        final String signature = Optional.ofNullable(node.get(2)).orElseThrow().asText();
-        final String payer = Optional.ofNullable(node.get(3)).orElseThrow().asText();
+        final String nonce = getText(node, 1);
+        final String signature = getText(node, 2);
+        final String payer = getText(node, 3);
+        final String spender = getText(node, 4);
 
         Permit2Tag.Permit2TagBuilder tag = Permit2Tag.builder().nonce(nonce).payer(payer)
-                .signature(signature);
+                .signature(signature).spender(spender);
 
         return (T) tag.build();
+    }
+
+    private static String getText(JsonNode node, int index) {
+        return Optional.ofNullable(node.get(index)).orElseThrow().asText();
     }
 }

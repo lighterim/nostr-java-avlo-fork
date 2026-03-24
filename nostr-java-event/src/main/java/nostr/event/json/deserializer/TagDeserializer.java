ee5af3b6cb71effb29fbffe89f870f153f58255a
dust <dust@live.com>
2026-03-24 09:56:54 +0800

    arbitrationTag

--- nostr-java-event/src/main/java/nostr/event/json/deserializer/TagDeserializer.java
+++ nostr-java-event/src/main/java/nostr/event/json/deserializer/TagDeserializer.java
@@ -44,6 +44,8 @@ public class TagDeserializer<T extends BaseTag> extends JsonDeserializer<T> {
                 case NIP77Event.LIMIT_TAG_CODE -> LimitTag.deserialize(node);
                 case NIP77Event.ESCROW_TAG_CODE -> EscrowTag.deserialize(node);
                 case NIP77Event.CREATED_BY_TAG_CODE -> CreatedByTag.deserialize(node);
+                case NIP77Event.ARBITRATION_TAG_CODE ->  ArbitrationTag.deserialize(node);
+                case NIP77Event.TLSN_PROOF_TAG_CODE -> TlsnProofTag.deserialize(node);
                 case NIP77Event.LEDGER_TAG_CODE -> LedgerTag.deserialize(node);
                 case NIP77Event.TRADE_KEY_TAG_CODE -> TradeKeyTag.deserialize(node);
                 case NIP77Event.PERMIT2_TAG_CODE -> Permit2Tag.deserialize(node);
